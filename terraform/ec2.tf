7e201f2f8eae8a4db637366fb7965af6ed212543
MYPC <yashwanthkotha147@gmail.com>
2021-08-23 18:25:39 +0530

    files are added...

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -21,3 +21,8 @@ resource "aws_route53_record" "records" {
 locals {
   LENGTH             = length(var.COMPONENTS)
 }
+
+resource "local_file" "foo" {
+  content     = "[FRONTEND]\n${aws_instance.instance.*.private_ip[9]}"
+  filename = "/tmp/inv-roboshop"
+}
